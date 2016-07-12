513d6d3515cde9dca5ac192926dec6ee439bc095
knpre <knpre@DESKTOP-KP4VUL2>
2016-07-12 09:47:25 +0300

    Try geolocation on win

--- SleepingBus/SleepingBus_UWP/newAlarmPage.xaml.cs
+++ SleepingBus/SleepingBus_UWP/newAlarmPage.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -25,12 +29,37 @@ namespace SleepingBus_UWP
         public newAlarmPage()
         {
             this.InitializeComponent();
-            
+            ShowMyLocationOnTheMap();
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
             Frame.GoBack();
+        }
+        private async void ShowMyLocationOnTheMap()
+        {
+            Geolocator myGeolocator = new Geolocator();
+            Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
+            /*
+            Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
+
+            BasicGeoposition BG = new BasicGeoposition();
+
+            BG.Latitude = myGeocoordinate.Latitude;
+            BG.Longitude = myGeocoordinate.Longitude;
+
+            Geopoint meOnMap = new Geopoint(BG);
+            */
+            stopMap.Center = myGeoposition.Coordinate.Point;
+            stopMap.ZoomLevel = 13;
+
+            MapIcon mapIcon = new MapIcon();
+            mapIcon.Image = RandomAccessStreamReference.CreateFromUri(
+              new Uri("ms-appx:///Assets/PinkPushPin.png"));
+            mapIcon.NormalizedAnchorPoint = new Point(0.25, 0.9);
+            mapIcon.Location = myGeoposition.Coordinate.Point;
+            mapIcon.Title = "You are here";
+            stopMap.MapElements.Add(mapIcon);
         }
     }
 }
